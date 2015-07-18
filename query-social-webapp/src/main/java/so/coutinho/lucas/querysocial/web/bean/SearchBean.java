1b0523f739de81b0e17f0f048e59498e4ee9dfd5
Lucas Oliveira <lucascs.oliveira@gmail.com>
2015-07-18 17:46:35 -0300

    Add dados das páginas retornadas
    Adicionados dados retornados do form de páginas retornadas da pesquisa.

--- query-social-webapp/src/main/java/so/coutinho/lucas/querysocial/web/bean/SearchBean.java
+++ query-social-webapp/src/main/java/so/coutinho/lucas/querysocial/web/bean/SearchBean.java
@@ -1,14 +1,21 @@
 package so.coutinho.lucas.querysocial.web.bean;
 
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Calendar;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.LineChartModel;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.context.WebApplicationContext;
+import so.coutinho.lucas.querysocial.facebook.FacebookWrapper;
 import so.coutinho.lucas.querysocial.facebook.Page;
+import so.coutinho.lucas.querysocial.util.Pair;
 
 /**
  *
@@ -20,8 +27,9 @@ import so.coutinho.lucas.querysocial.facebook.Page;
 @Scope(WebApplicationContext.SCOPE_SESSION)
 public class SearchBean {
 
-    private Date startDate;
-    private Date endDate;
+    private Calendar startDate;
+    private Calendar endDate;
+    private FacebookWrapper facebookWrapper;
     private List<Page> pages = new ArrayList<>();
     private List<Page> selectedPages = new ArrayList<>();
 
--- query-social-webapp/src/main/java/so/coutinho/lucas/querysocial/web/controller/SearchController.java
+++ query-social-webapp/src/main/java/so/coutinho/lucas/querysocial/web/controller/SearchController.java
@@ -2,6 +2,8 @@ package so.coutinho.lucas.querysocial.web.controller;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 import so.coutinho.lucas.querysocial.web.bean.ContextUrls;
 import javax.servlet.http.HttpSession;
@@ -28,7 +30,7 @@ import so.coutinho.lucas.querysocial.web.bean.SessionAttributes;
 public class SearchController extends AbstractController {
 
     @Autowired
-    SearchBean searchBean;
+    private SearchBean searchBean;
 
     @RequestMapping(method = RequestMethod.GET)
     public String loadForm(HttpSession session, ModelMap model) {
@@ -50,16 +52,47 @@ public class SearchController extends AbstractController {
 
     @RequestMapping(value = ContextUrls.RESULT, method = RequestMethod.POST)
     public String getResult(HttpSession session, ModelMap model, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, @RequestParam("selectedPages") String selectedPages) {
-        System.out.println(startDate);
-        System.out.println(endDate);
-        System.out.println(selectedPages);
+        searchBean.setStartDate(stringToCalendar(startDate));
+        searchBean.setEndDate(stringToCalendar(endDate));
+        searchBean.setFacebookWrapper((FacebookWrapper) session.getAttribute(SessionAttributes.FB_SESSION));
+        List<String> ids = stringToList(selectedPages);
 
+        for (String id : ids) {
+            for (Page page : searchBean.getPages()) {
+                if (page.getId().equals(id)) {
+                    searchBean.getSelectedPages().add(page);
+                    break;
+                }
+            }
+        }
+
+        //TODO: Insert Json with data for chart
         return doFilter(session, "search-result");
     }
 
     private void clearBean() {
         searchBean.getPages().clear();
         searchBean.getSelectedPages().clear();
+    }
+
+    private Calendar stringToCalendar(String date) {
+        String[] dateFields = date.split("/");
+
+        return new GregorianCalendar(Integer.parseInt(dateFields[2]), Integer.parseInt(dateFields[1]) - 1, Integer.parseInt(dateFields[0]));
+    }
+
+    private List<String> stringToList(String ids) {
+        List<String> listIds;
+
+        if (ids.equals("[]")) {
+            listIds = new ArrayList<>();
+        } else {
+            ids = ids.replace("[\"", "");
+            ids = ids.replace("\"]", "");
+
+            listIds = Arrays.asList(ids.split("\",\""));
+        }
+        return listIds;
     }
 
 }
