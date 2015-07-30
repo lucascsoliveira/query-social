91ad07406f10ede179a341307f5fa0834a847260
Lucas Oliveira <lucascs.oliveira@gmail.com>
2015-07-29 23:27:35 -0300

    Add Scope Session
    Adicionada a annotation @Scope(WebApplicationContext.SCOPE_SESSION) em SearchBean.

--- query-social-webapp/src/main/java/so/coutinho/lucas/querysocial/web/bean/SearchBean.java
+++ query-social-webapp/src/main/java/so/coutinho/lucas/querysocial/web/bean/SearchBean.java
@@ -5,7 +5,9 @@ import java.util.Calendar;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 import so.coutinho.lucas.querysocial.facebook.FacebookWrapper;
 import so.coutinho.lucas.querysocial.facebook.Page;
 
@@ -16,6 +18,7 @@ import so.coutinho.lucas.querysocial.facebook.Page;
 @Getter
 @Setter
 @Component
+@Scope(WebApplicationContext.SCOPE_SESSION)
 public class SearchBean {
 
     private Calendar startDate;
